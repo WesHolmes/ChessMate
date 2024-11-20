45ca96df35c8815414a5203551c9822413426fbe
WesHolmes <wholmes1@lion.lmu.edu>
2024-11-20 13:10:53 -0800

    adding firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import Nav from "./Nav"
+import Article from "./Article"
+import ArticleEntry from "./ArticleEntry"
+import { SignIn, SignOut } from "./Auth"
+import { useAuthentication } from "../services/authService"
+import { fetchArticles, createArticle } from "../services/articleService"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [articles, setArticles] = useState([])
+  const [article, setArticle] = useState(null)
+  const [writing, setWriting] = useState(false)
+  const user = useAuthentication()
+
+  // This is a trivial app, so just fetch all the articles only when
+  // a user logs in. A real app would do pagination. Note that
+  // "fetchArticles" is what gets the articles from the service and
+  // then "setArticles" writes them into the React state.
+  useEffect(() => {
+    if (user) {
+      fetchArticles().then(setArticles)
+    }
+  }, [user])
+
+  // Update the "database" *then* update the internal React state. These
+  // two steps are definitely necessary.
+  function addArticle({ title, body }) {
+    createArticle({ title, body }).then((article) => {
+      setArticle(article)
+      setArticles([article, ...articles])
+      setWriting(false)
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Brawl Stats</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header>
+        Blog
+        {user && <button onClick={() => setWriting(true)}>New Article</button>}
+        {!user ? <SignIn /> : <SignOut />}
+      </header>
+
+      {!user ? "" : <Nav articles={articles} setArticle={setArticle} />}
+
+      {!user ? (
+        ""
+      ) : writing ? (
+        <ArticleEntry addArticle={addArticle} />
+      ) : (
+        <Article article={article} />
+      )}
+    </div>
   )
 }
-
-export default App
