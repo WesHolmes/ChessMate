12b44718d4b0a60a588bd9b070c4d2350c77cbe6
WesHolmes <wholmes1@lion.lmu.edu>
2024-12-03 14:42:37 -0800

    commiting new code

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Search from "./Search"
+import Results from "./Results"
+
+function App() {
+  return (
+    <>
+      <h1>Chess app</h1>
+      <Search/>
+      <Results/>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function Search () {
+    const [term, setTerm] = useState("")
+
+    function submit(e) {
+        e.preventDefault()
+        console.log(term)
+    }
+
+    return(
+        <form onSubmit={submit}>
+            <input type = "text" 
+            placeholder="Search..."
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            />
+        </form>
+
+    )
+}
